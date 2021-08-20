9d2e5c261dae71f0d1841c92810f08322f8f31a1
francescodesimone <f.desimone30@studenti.unisa.it>
2021-08-20 14:48:50 +0200

    Start Working on sorting algorithms

--- src/main.rs
+++ src/main.rs
@@ -16,39 +16,70 @@ use tui::{
     widgets::{BarChart, Block, Borders},
     Terminal,
 };
+use rand::thread_rng;
+use rand::seq::SliceRandom;
 
 struct App<'a> {
     data: Vec<(&'a str, u64)>,
     items: StatefulList<(&'a str, usize)>,
+    sorting: bool,
 }
 
 impl<'a> App<'a> {
     fn new() -> App<'a> {
-        App {
+      let mut app = App {
             data: (0..100).map(|x| ("", x)).collect::<Vec<_>>(),
-            items: StatefulList::with_items(vec![("Bouble Sort", 1), ("Insertion Sort", 2)]),
+            items: StatefulList::with_items(vec![("Bubble Sort", 1), ("Insertion Sort", 2)]),
+            sorting: false,
+        };
+      app.shuffle_data();
+      app
+    }
+
+    fn shuffle_data(&mut self){
+      self.data.shuffle(&mut thread_rng());
+    }
+
+    fn update(&mut self) {
+        match self.items.state.selected() {
+          Some(_) => {
+            if self.sorting {
+              match self.items.items[self.items.state.selected().unwrap()].0 {
+                "Bubble Sort" => {self.BubbleSort(self.data.len())},
+                "Insertion Sort" => {}
+                _ => {}
+              }
+            }
+          },
+          None => {}
         }
     }
 
-    fn update(&mut self) {}
+    fn BubbleSort(&mut self, n:usize){
+      if n == 1 {
+        self.sorting = false;
+        return;
+      } 
+      for i in 0..n-1 {
+        if self.data[i].1 > self.data[i+1].1{
+          self.data.swap(i+1,i);
+        }
+      }
+      std::thread::sleep(std::time::Duration::from_millis(10));
+     self.BubbleSort(n-1);
+    }
+
 }
 
-struct SortGenerator { }
-
-impl
-
 fn main() -> Result<(), Box<dyn Error>> {
-    // Terminal initialization
     let stdout = io::stdout().into_raw_mode()?;
     let stdout = MouseTerminal::from(stdout);
     let stdout = AlternateScreen::from(stdout);
     let backend = TermionBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
 
-    // Setup event handlers
     let events = Events::new();
 
-    // App
     let mut app = App::new();
 
     loop {
@@ -100,9 +131,8 @@ fn main() -> Result<(), Box<dyn Error>> {
                 Key::Up => {
                     app.items.previous();
                 }
-                Key::Char(' ') => {
-                  
-                },
+                Key::Char(' ') => {app.sorting = ! app.sorting}
+                Key::Char('r') => {app.shuffle_data()}
                 _ => {}
             },
             Event::Tick => {
